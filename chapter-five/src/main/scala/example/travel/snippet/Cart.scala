777c2c5af69814edd221195176ced4d7dc31b29f
Timothy Perrett <hello@timperrett.com>
2010-06-22 00:32:31 +0100

    Laying chapter five foundations

---
+++ chapter-five/src/main/scala/example/travel/snippet/Cart.scala
@@ -0,0 +1,13 @@
+package example {
+package travel {
+package snippet {
+  
+  import net.liftweb.http.DispatchSnippet
+  import scala.xml.NodeSeq
+  // import net.liftweb.
+  
+  class Cart extends DispatchSnippet {
+    // def items(xhtml: NodeSeq): NodeSeq = 
+  }
+  
+}}}

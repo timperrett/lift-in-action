d500b05596a4562942cdb7e9f22078d6bf7ef942
Timothy Perrett <timothy@getintheloop.eu>
2011-01-11 22:40:38 +0000

    Added some more squeryl stuff

--- chapter-12/src/main/scala/sample/model/squeryl/Schema.scala
+++ chapter-12/src/main/scala/sample/model/squeryl/Schema.scala
@@ -1,9 +1,23 @@
 package sample.model.squeryl
 
 import org.squeryl.Schema
+import org.squeryl.PrimitiveTypeMode._
+
 
 object Bookstore extends Schema {
   val authors = table[Author]("authors")
+  on(authors)(a => declare(
+    a.id is(primaryKey,autoIncremented)
+  ))
+  
   val books = table[Book]("books")
+  on(books)(b => declare(
+    b.id is(primaryKey,autoIncremented)
+  ))
+  
   val publishers = table[Publisher]("publishers")
+  on(publishers)(p => declare(
+    p.id is(primaryKey,autoIncremented)
+  ))
+  
 }

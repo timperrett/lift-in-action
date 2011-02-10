ab5713dc81f99442ed9714481f35a4b12adfc29d
timperrett <hello@timperrett.com>
2011-02-10 23:00:14 +0000

    Finally got the akka + lift actors with futures example working

--- chapter-13/src/main/scala/sample/comet/Calculator.scala
+++ chapter-13/src/main/scala/sample/comet/Calculator.scala
@@ -3,26 +3,7 @@ package sample.comet
 import akka.actor.Actor
 import akka.actor.Actor._
 
-// incoming messages
-// sealed trait Operation {
-//   val sign: String
-// }
-// object Operation {
-//   def fromSign(sign: String) = 
-// }
-// case object Multiply extends Operation {
-//   val sign = "*"
-// }
-// case object Add extends Operation {
-//   val sign = "+"
-// }
-// case object Divide extends Operation {
-//   val sign = "/"
-// }
 sealed case class Compute(thiz: Double, that: Double, by: String)
-
-// response
-// sealed case class Result(value: Double)
 
 class Calculator extends Actor {
   def receive = {
@@ -51,26 +32,20 @@ class CalculatorDisplay extends CometActor {
   private var two = 0D
   private var operation: Box[String] = Empty
   
-  def render = SHtml.ajaxForm(
-    bind("f", defaultXml,
-      "value_one" -> SHtml.text(one.toString, a => one = asDouble(a).openOr(0D)),
-      "value_two" -> SHtml.text(two.toString, b => two = asDouble(b).openOr(0D)),
-      "operation" -> SHtml.select(Seq("+","-","*").map(x => (x -> x)), operation, v => operation = Full(v)),
-      "submit" -> SHtml.submit("Submit", () => {
-        println("vals: %s, %s, %s".format(one,two,operation))
-        val future: Future[Double] = registry.actorFor[Calculator].get !!! Compute(one,two,operation.openOr("+"))
-        future.onComplete(f => this ! f.result)
-      })
-    )
-  )
-
-  //"#clock_time *" replaceWith timeNow.toString
+  def render = SHtml.ajaxForm(bind("f",
+    "value_one" -> SHtml.text(one.toString, a => one = asDouble(a).openOr(0D)),
+    "value_two" -> SHtml.text(two.toString, b => two = asDouble(b).openOr(0D)),
+    "operation" -> SHtml.select(Seq("+","/","*").map(x => (x -> x)), operation, v => operation = Full(v)),
+    "submit" -> SHtml.ajaxSubmit("Submit", () => {
+      lazy val future: Future[Double] = registry.actorFor[Calculator].get !!! Compute(one,two,operation.openOr("+"))
+      future.onComplete(f => this ! f.result)
+      Noop
+    })
+  ))
   
   override def lowPriority = {
-    case Some(value: Double) => {
-      println("RESPONSE: " + value.toString)
+    case Some(value: Double) => 
       partialUpdate(SetHtml("result", Text(value.toString)))
-    }
   }
 }
 

927bb8ddc071c9f3641da3d2502bc4593694bbe2
timperrett <hello@timperrett.com>
2011-02-10 17:34:01 +0000

    Updted examples

--- chapter-13/src/main/scala/sample/comet/Calculator.scala
+++ chapter-13/src/main/scala/sample/comet/Calculator.scala
@@ -1,13 +1,25 @@
 package sample.comet
 
 import akka.actor.Actor
+import akka.actor.Actor._
 
 // incoming messages
-sealed trait Operation
-case object Multiply extends Operation
-case object Add extends Operation
-case object Divide extends Operation
-sealed case class Compute(thiz: Double, that: Double, by: Operation)
+// sealed trait Operation {
+//   val sign: String
+// }
+// object Operation {
+//   def fromSign(sign: String) = 
+// }
+// case object Multiply extends Operation {
+//   val sign = "*"
+// }
+// case object Add extends Operation {
+//   val sign = "+"
+// }
+// case object Divide extends Operation {
+//   val sign = "/"
+// }
+sealed case class Compute(thiz: Double, that: Double, by: String)
 
 // response
 sealed case class Result(value: Double)
@@ -15,36 +27,51 @@ sealed case class Result(value: Double)
 class Calculator extends Actor {
   def receive = {
     case Compute(a,b,by) => by match {
-      case Multiply => a * b
-      case Add => a + b
-      case Divide => a / b
+      case "*" => a * b
+      case "+" => a + b
+      case "/" => a / b
     }
   }
 }
 
 import scala.xml.{NodeSeq,Text}
+import net.liftweb.common.{Box,Full,Empty}
 import net.liftweb.util.Helpers._
 import net.liftweb.http.{CometActor,SHtml}
 import net.liftweb.http.js.JsCmds.SetHtml
+import akka.actor.Actor.registry
 
 class CalculatorDisplay extends CometActor {
   private var one = 0D
   private var two = 0D
+  private var operation: Box[String] = Empty
   
-  def render = 
+  def render = {
     "#value_one" #> SHtml.text(one.toString, a => one = asDouble(a).openOr(0D)) & 
-    "#value_two" #> SHtml.text(two.toString, b => two = asDouble(b).openOr(0D)) &
-    "select" #> SHtml.select()
-    "type=submit" #> SHtml.submit("Calculate!", () => {})
+    "form" #>((ns: NodeSeq) => SHtml.ajaxForm(ns)) //&
+    // "#value_one" #> SHtml.text(one.toString, a => one = asDouble(a).openOr(0D)) & 
+    // "#value_two" #> SHtml.text(two.toString, b => two = asDouble(b).openOr(0D)) //&
+    // "name=value_operation" #> SHtml.select(
+    //   Seq("+","-","*").map(x => (x -> x)), 
+    //   operation, v => operation = Full(v)) &
+    // "type=submit" #> SHtml.submit("Calculate!", () => {
+    //   println("vals: %s, %s, %s".format(one,two,operation))
+    //   val future = registry.actorFor[Calculator].get !!! Compute(one,two,operation.openOr("+"))
+    //   future.onComplete(f => this ! f.result)
+    // })
+  }
   
+    
+//       
+
   //"#clock_time *" replaceWith timeNow.toString
   
   override def lowPriority = {
-    case Result(value) =>
-      partialUpdate(SetHtml("clock_time", Text(timeNow.toString)))
+    case Result(value) => println(value)
+      // partialUpdate(SetHtml("clock_time", Text(timeNow.toString)))
   }
   
-  //(Actor.actorFor[Calculator] !!! Compute(a,b,o)).onComplete(f => this ! f.result)
+  //
   
 }
 

a8c07ea89731d5ab7813fe43f4bdca4784ff7b4d
timperrett <hello@timperrett.com>
2011-02-11 01:11:34 +0000

    Swapped exampl to CSS selectors as its nicer

--- chapter-13/src/main/scala/sample/comet/Calculator.scala
+++ chapter-13/src/main/scala/sample/comet/Calculator.scala
@@ -1,7 +1,6 @@
 package sample.comet
 
 import akka.actor.Actor
-import akka.actor.Actor._
 
 sealed case class Compute(thiz: Double, that: Double, by: String)
 
@@ -23,25 +22,24 @@ import scala.xml.{NodeSeq,Text}
 import net.liftweb.common.{Box,Full,Empty}
 import net.liftweb.util.Helpers._
 import net.liftweb.http.{CometActor,SHtml}
-import net.liftweb.http.js.JsCmds.{SetHtml,Noop,Alert}
+import net.liftweb.http.js.JsCmds.{SetHtml,Noop}
 import akka.actor.Actor.registry
 import akka.dispatch.Future
 
 class CalculatorDisplay extends CometActor {
-  private var one = 0D
-  private var two = 0D
+  private var one, two = 0D
   private var operation: Box[String] = Empty
   
-  def render = SHtml.ajaxForm(bind("f",
-    "value_one" -> SHtml.text(one.toString, a => one = asDouble(a).openOr(0D)),
-    "value_two" -> SHtml.text(two.toString, b => two = asDouble(b).openOr(0D)),
-    "operation" -> SHtml.select(Seq("+","/","*").map(x => (x -> x)), operation, v => operation = Full(v)),
-    "submit" -> SHtml.ajaxSubmit("Submit", () => {
+  def render = 
+    "#value_one" #> SHtml.text(one.toString, a => one = asDouble(a).openOr(0D)) &
+    "#value_two" #> SHtml.text(two.toString, b => two = asDouble(b).openOr(0D)) &
+    "#operation" #> SHtml.select(Seq("+","/","*").map(x => (x -> x)), operation, v => operation = Full(v)) &
+    "type=submit" #> SHtml.ajaxSubmit("Submit", () => {
       lazy val future: Future[Double] = registry.actorFor[Calculator].get !!! Compute(one,two,operation.openOr("+"))
       future.onComplete(f => this ! f.result)
       Noop
-    })
-  ))
+    }) andThen SHtml.makeFormsAjax
+    
   
   override def lowPriority = {
     case Some(value: Double) => 

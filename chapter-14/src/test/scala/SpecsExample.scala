5367dbbc99df00c43a927af16651f80791c343f7
timperrett <hello@timperrett.com>
2011-02-20 23:02:22 +0000

    Added scala check sample

--- chapter-14/src/test/scala/SpecsExample.scala
+++ chapter-14/src/test/scala/SpecsExample.scala
@@ -12,3 +12,32 @@ class SpecsExample extends Specification {
     }
   }
 }
+
+trait SetupAndTearDown {
+  def construcEnvironment() = 
+    println("Construcing the environment!")
+  def tearDownEnvironment() = 
+    println("Tearing down the environment!")
+}
+
+class AnotherExample extends Specification with SetupAndTearDown {
+  construcEnvironment().beforeSpec
+  "An Option" should {
+    "Be None if supplied a null value" in {
+      Option(null) must_== None
+    }
+  }
+  tearDownEnvironment().afterSpec
+}
+
+import org.scalacheck.Prop._
+
+class ScalaCheckExample extends Specification with ScalaCheck {
+  "Strings" should {
+    "Start with" in { 
+      forAll {
+        (a: String, b: String) => (a + b).startsWith(a)
+      } must_== true
+    }
+  }
+}

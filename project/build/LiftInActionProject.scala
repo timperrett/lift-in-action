48f1203d18e07b528acefcc2e99148039ea3124f
timperrett <hello@timperrett.com>
2011-02-22 11:09:56 +0000

    Moved SBT project to use module configurations to vastly speed up the update time

--- project/build/LiftInActionProject.scala
+++ project/build/LiftInActionProject.scala
@@ -174,15 +174,39 @@ class LiftInActionProject(info: ProjectInfo) extends ParentProject(info) with Id
     val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.21" % "test"
     // val log4j = "org.slf4j" % "slf4j-log4j12" % "1.6.1" % "compile"
     
-    // repositories
-    lazy val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
-    lazy val mavenCentral = "Central Maven Repository" at "http://repo1.maven.org/maven2/"
-    lazy val jbossRepo = "jboss.repo" at "http://repository.jboss.org/nexus/content/groups/public-jboss/"
-    lazy val twitterRepo = "twitter.repo" at "http://maven.twttr.com/"
-    lazy val scalaReleases = "scala-tools.releases" at "http://scala-tools.org/repo-releases/"
-    lazy val scalaSnapshots = "scala-tools.snapshots" at "http://scala-tools.org/repo-snapshots/"
-    lazy val sonatype = "oss.sonatype.org" at "http://oss.sonatype.org/content/groups/github/"
-    lazy val akkarepo = "akka.repo" at "http://akka.io/repository/"
-    lazy val guiceyFruitRepo = "GuiceyFruit Repo" at "http://guiceyfruit.googlecode.com/svn/repo/releases/"
+    object Repositories {
+      lazy val MavenLocal           = MavenRepository("local.repo", "file://"+Path.userHome+"/.m2/repository")
+      lazy val MavenCentral         = MavenRepository("central.repo", "http://repo1.maven.org/maven2/")
+      lazy val JBossRepo            = MavenRepository("jboss.repo", "http://repository.jboss.org/nexus/content/groups/public-jboss/")
+      lazy val TwitterRepo          = MavenRepository("twitter.repo", "http://maven.twttr.com/")
+      lazy val ScalaToolsReleases   = MavenRepository("scala-tools.releases", "http://scala-tools.org/repo-releases/")
+      lazy val ScalaToolsSnapshots  = MavenRepository("scala-tools.snapshots", "http://scala-tools.org/repo-snapshots/")
+      lazy val SonatypeRepo         = MavenRepository("oss.sonatype.org", "http://oss.sonatype.org/content/groups/github/")
+      lazy val AkkaRepo             = MavenRepository("akka.repo", "http://akka.io/repository/")
+      lazy val GuiceyFruitRepo      = MavenRepository("guiceyfruit.repo", "http://guiceyfruit.googlecode.com/svn/repo/releases/")
+    }
+    
+    import Repositories._
+    // module Configurations
+    lazy val localMavenRepo           = MavenLocal
+    lazy val liftModuleConfig         = ModuleConfiguration("net.liftweb", ScalaToolsReleases)
+    lazy val scalaTestModuleConfig    = ModuleConfiguration("org.scalatest", ScalaToolsReleases)
+    lazy val specsModuleConfig        = ModuleConfiguration("org.scala-tools.testing", ScalaToolsReleases)
+    lazy val akkaModuleConfig         = ModuleConfiguration("se.scalablesolutions.akka", AkkaRepo)
+    lazy val uuidModuleConfig         = ModuleConfiguration("com.eaio", AkkaRepo)
+    lazy val sjsonModuleConfig        = ModuleConfiguration("sjson.json", AkkaRepo)
+    lazy val nettyModuleConfig        = ModuleConfiguration("org.jboss.netty", JBossRepo)
+    lazy val aspeketModuleConfig      = ModuleConfiguration("org.codehaus.aspectwerkz", AkkaRepo)
+    lazy val twitterModuleConfig      = ModuleConfiguration("com.twitter", TwitterRepo)
+    lazy val lagNetCustModuleConfig   = ModuleConfiguration("net.lag", "configgy", "2.0.2-nologgy", AkkaRepo)
+    lazy val lagNetModuleConfig       = ModuleConfiguration("net.lag", "configgy", "2.0.2", TwitterRepo)
+    lazy val hibernateModuleConfig    = ModuleConfiguration("org.hibernate", JBossRepo)
+    lazy val javaxPersModuleConfig    = ModuleConfiguration("org.hibernate.javax.persistence", JBossRepo)
+    lazy val jettyModuleConfig        = ModuleConfiguration("org.eclipse.jetty", DefaultMavenRepository)
+    lazy val guiceyFruitModuleConfig  = ModuleConfiguration("org.guiceyfruit", GuiceyFruitRepo)
+    lazy val jbossModuleConfig        = ModuleConfiguration("org.jboss", JBossRepo)
+    lazy val logbackModuleConfig      = ModuleConfiguration("ch.qos.logback", sbt.DefaultMavenRepository)
   }
 }
+
+
